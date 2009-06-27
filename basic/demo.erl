d8d6326a8b7b001f7ade54b33587042a6ece14c5
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-06-27 15:24:03 +0800

    what an Erlang module looks like

---
+++ basic/demo.erl
@@ -0,0 +1,13 @@
+% Erlang Programming, p. 40
+
+-module(demo).
+-export([double/1]).
+
+% This is a comment
+% Everything on a line after % is ignored.
+
+double(Value) ->
+  times(Value, 2).
+
+times(X, Y) ->
+  X * Y.

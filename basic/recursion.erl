cc02a5a424eac71ec295b249ca667024dd5f3371
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-06-27 18:28:23 +0800

    more recursion examples

--- basic/recursion.erl
+++ basic/recursion.erl
@@ -1,10 +1,13 @@
 % Recursion example (Erlang Programming, p. 59)
 
-% Adds 1 to each element of a list.
-
--module(bump).
+-module(recursion).
 
 -export([bump/1]).
+-export([member/2]).
 
 bump([]) -> [];
 bump([Head | Tail]) -> [Head + 1 | bump(Tail)].
+
+member(_, [])    -> false;
+member(H, [H|_]) -> true;
+member(H, [_|T]) -> member(H, T).

f9e59530827514e55c33334ffbaf0b3699fdbd81
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-07-07 21:23:13 +0800

    implemented match
    used splicing for delete

--- patterns/my_db.erl
+++ patterns/my_db.erl
@@ -4,14 +4,18 @@
 
 -export([start/0, stop/0]).
 -export([loop/1]).
--export([write/2, read/1, delete/1]).
+-export([write/2, read/1, delete/1, match/1]).
 
 start() ->
   State = [],
   register(my_db, spawn(my_db, loop, [State])).
 
 stop() ->
-  my_db ! stop.
+  my_db ! {stop, self()},
+  receive
+    {reply, Reply} ->
+      Reply
+  end.
 
 write(Key, Element) ->
   my_db ! {write, self(), Key, Element},
@@ -34,6 +38,13 @@ delete(Key) ->
       Reply
   end.
 
+match(Element) ->
+  my_db ! {match, self(), Element},
+  receive
+    {reply, Reply} ->
+      Reply
+  end.
+
 
 loop(Data) ->
   receive
@@ -49,20 +60,27 @@ loop(Data) ->
       Result = find(Key, Data),
       reply(Pid, {reply, Result}),
       loop(Data);
-    stop ->
-      ok
+    {match, Pid, Element} ->
+      Result = match(Element, Data, []),
+      reply(Pid, {reply, Result}),
+      loop(Data);
+    {stop, Pid} ->
+      reply(Pid, {reply, ok})
   end.
 
 write(Key, Element, Data) ->
   {ok, NewData} = delete(Key, Data),
   {ok, [{Key, Element} | NewData]}.
 
-delete(_Key, []) ->
-  {ok, []};
-delete(Key, [{Key, _Element} | Data]) ->
-  {ok, Data};
-delete(Key, [_H | Data]) ->
-  delete(Key, Data).
+delete(Key, Data) ->
+  delete(Key, Data, []).
+
+delete(_Key, [], R) ->
+  {ok, R};
+delete(Key, [{Key, _Element} | T], R) ->
+  {ok, R ++ T}; % we only have one key to match
+delete(Key, [Other | T], R) ->
+  delete(Key, T, [Other | R]).
 
 find(_Key, []) ->
   {error, instance};
@@ -73,3 +91,10 @@ find(Key, [_H | Data]) ->
 
 reply(Pid, Msg) ->
   Pid ! Msg.
+
+match(_Element, [], Result) ->
+  Result;
+match(Element, [{Key, Element} | Data], Result) ->
+  match(Element, Data, [Key | Result]);
+match(Element, [_H | Data], Result) ->
+  match(Element, Data, Result).
